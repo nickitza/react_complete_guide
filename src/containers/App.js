8603b1229a5b80d014cc05f715579d71dfd3cddf
Nicki Nixon <nickitza@gmail.com>
2019-09-11 10:30:32 -0600

    folder/file restructure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 import './App.css';
-import Person from './Person/Person';
+import Person from '../components/Persons/Person/Person'
 import Radium, {StyleRoot} from 'radium';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
+import ErrorBoundary from '../ErrorBoundary/ErrorBoundary'
 
 
 class App extends Component {
