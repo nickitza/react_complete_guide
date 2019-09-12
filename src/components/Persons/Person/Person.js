298b8508b881ab2317ed0324606b8130671b49d2
Nicki Nixon <nickitza@gmail.com>
2019-09-12 17:02:17 -0600

    add aux

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Person.css'
+import Aux from '../../../hoc/Aux'
 
 
 const Person = ( props ) => {
   return(
-    <div className="Person">
+    <Aux>
       <p onClick={props.click} >I'm {props.name}, I'm {props.age} years old.</p>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
+    </Aux>
     )
 }
 
---
+++ src/hoc/Aux.js
@@ -0,0 +1,5 @@
+//children is a reserve property name
+//props.children special simply outputs whatever is output in between the opening and closing tag in a component
+const aux = props => props.children
+
+export default aux
